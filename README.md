b72172ba8c95d4641d7875b74afa26325007cbfb
Dimitris Dam <dimitrisdamo@gmail.com>
2019-05-29 12:49:48 +0200

    DimitriosTask
    Dimitrios Ntamotsidis Widget

--- README.md
+++ README.md
@@ -1,43 +1,29 @@
-# Junior Frontend Developer
+This is a Weather App that used Weather API .
 
-## Task
 
-Write a simple "widget" that uses JavaScript to request data from an external
-weather API, and visualize it inside the Browser.
+Technologies Used :
+HTML 
+Javascript 
+CSS
 
-For ease of use, we suggest to use this public API, as it doesn't need an app
-registration (no key needed):
 
-[Weather API](https://fcc-weather-api.glitch.me/)
+###Requirements
+Get the location of the user
+Get the weather data for the location of the user
+Write a "widget" that is showing the following information
+Weather Location
+current temperature
+current cloudiness with an svg-icon representing this information (sun, sun & clouds, rain and so on)
+current wind.
 
-...but if you want to use anything else, please feel free to do so! :)
 
-But please do **NOT** use a framework or library for the implementation of any
-part of this task!
+Screenshots
 
-### FYI:
 
-Of course we are using frameworks and libraries here at flagbit... many of them.
-But this task is about getting to know **YOU**, how you are coding and what your
-personal opinion or solution for specific technical questions/problems is.
+APIs / Libraries/References 
 
-### Requirements
+-[Weather API](https://fcc-weather-api.glitch.me/)
+-[Icons](https://erikflowers.github.io/weather-icons/)
+-JSON
 
-1. Get the location of the user
-2. Get the weather data for the location of the user
-3. Write a "widget" that is showing the following information
-   - Weather Location
-   - current temperature
-   - current cloudiness with an svg-icon representing this information (sun, sun & clouds, rain and so on)
-   - current wind
 
-You don't have to take too much care about cross-browser testing. If its working in
-the latest versions of chrome and firefox everything is fine :)
-
-## Questions
-
-- How could you request data inside the browser, from another domain?
-- What does CORS stand for?
-- Please name 5 different HTTP methods and explain what they are used for.
-- What is the difference between 'DOMContentLoaded' and 'Load' events?
-- What is the DOM?
---
+++ js/widgetJs.js
@@ -0,0 +1,103 @@
+﻿//Getting permission from User or Not
+if (navigator.geolocation) {				
+ navigator.geolocation.getCurrentPosition(getData);
+} else {
+ alert('Geolocation is not supported in your browser');
+}
+
+
+
+
+function getData(position) {
+  //Get Latitude and Longitude
+  latitude =position.coords.latitude;
+  longitude =position.coords.longitude;
+  
+  var  apiUrl = "https://cors-anywhere.herokuapp.com/https://fcc-weather-api.glitch.me/api/current?lat=" + latitude + "&lon=" + longitude;
+
+  $.getJSON(apiUrl,function(data){
+    // Getting and Setting data from apiUrl to HTML entities 
+    $("#place").html(data.name + ", " + data.sys.country);
+    $("#temp").html(data.main.temp.toFixed(1)+ " °C");
+	$("#wind").html(data.wind.speed);
+    
+		
+		//Set Weather Speed Icon
+		var speed=data.wind.speed;
+		
+		if(speed<1){
+			$("#windSpeedIcon").html("<object id='windIcon' data='../svg/wind/wi-wind-beaufort-0.svg' width='40' ></object>");
+		}
+		else if(speed<2){
+			$("#windSpeedIcon").html("<object style='color:white' id='windIcon' data='../svg/wind/wi-wind-beaufort-1.svg' width='40' ></object>");
+		}
+		else if(speed<3){
+			$("#windSpeedIcon").html("<object id='windIcon' data='../svg/wind/wi-wind-beaufort-2.svg' width='40' ></object>");
+		}
+		else if(speed<4){
+			$("#windSpeedIcon").html("<object id='windIcon' data='../svg/wind/wi-wind-beaufort-3.svg' width='40' ></object>");
+		}
+		else if(speed<5){
+			$("#windSpeedIcon").html("<object id='windIcon' data='../svg/wind/wi-wind-beaufort-4.svg' width='40' ></object>");
+		}
+		else if(speed<6){
+			$("#windSpeedIcon").html("<object id='windIcon' data='../svg/wind/wi-wind-beaufort-5.svg' width='40' ></object>");
+		}
+		else if(speed<7){
+			$("#windSpeedIcon").html("<object id='windIcon' data='../svg/wind/wi-wind-beaufort-6.svg' width='40' ></object>");
+		}
+		else if(speed<8){
+			$("#windSpeedIcon").html("<object id='windIcon' data='../svg/wind/wi-wind-beaufort-7.svg' width='40' ></object>");
+		}
+		else if(speed<9){
+			$("#windSpeedIcon").html("<object id='windIcon' data='../svg/wind/wi-wind-beaufort-8.svg' width='40' ></object>");
+		}
+		else if(speed<10){
+			$("#windSpeedIcon").html("<object id='windIcon' data='../svg/wind/wi-wind-beaufort-9.svg' width='40' ></object>");
+		}
+		else if(speed<11){
+			$("#windSpeedIcon").html("<object id='windIcon' data='../svg/wind/wi-wind-beaufort-10.svg' width='40' ></object>");
+		}
+		else if(speed<12){
+			$("#windSpeedIcon").html("<object id='windIcon' data='../svg/wind/wi-wind-beaufort-11.svg' width='40' ></object>");
+		}
+		else if(speed<13){
+			$("#windSpeedIcon").html("<object id='windIcon' data='../svg/wind/wi-wind-beaufort-12.svg' width='40' ></object>");
+		}
+		else{
+			$("#windSpeedIcon").html("<object id='windIcon' data='../svg/wind/wi-na.svg' width='40' ></object>");
+		}
+		
+		//Set Weather Cloud Icon 
+		var weatherDescription=data.main.description;
+		
+		if(weatherDescription='Clouds'){
+			$("#weatherIcon").html("<object id='description' data='../svg/description/wi-cloudy.svg' width='40' ></object>");
+		}
+		else if(weatherDescription='Drizzle'){
+			$("#weatherIcon").html("<object id='description' data='../svg/description/wi-raindrops.svg' width='40' ></object>");
+		}
+		else if(weatherDescription='Rain'){
+			$("#weatherIcon").html("<object id='description' data='../svg/description/wi-rain.svg' width='40' ></object>");
+		}
+		else if(weatherDescription='Snow'){
+			$("#weatherIcon").html("<object id='description' data='../svg/description/wi-snow.svg' width='40' ></object>");
+		}
+		else if(weatherDescription='Clear'){
+			$("#weatherIcon").html("<object id='description' data='../svg/description/wi-day-sunny.svg' width='40' ></object>");
+		}
+		else if(weatherDescription='Thunderstorm'){
+			$("#weatherIcon").html("<object id='description' data='../svg/description/wi-thunderstorm.svg' width='40' ></object>");
+		}
+		else if(weatherDescription='Mist'){
+			$("#weatherIcon").html("<object id='description' data='../svg/description/wi-day-haze.svg' width='40' ></object>");
+		}
+		else{
+			$("#windSpeedIcon").html("<object id='description' data='../svg/wind/wi-na.svg' width='40' ></object>");
+		}
+		
+  });
+}
+
+
+                                           
