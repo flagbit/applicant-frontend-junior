6deb105232076fdac94bb4bff207885f31d27b65
RebeccaSeraphine <rebecca.s.mueller@web.de>
2019-09-20 17:10:35 +0200

    geolocation retrieved for weather url

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-fetch("https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139")
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-  })
-  .catch(err => {
-    console.log("error");
-  });
+function userLocation() {
+  var location = document.getElementById("location");
+
+  var url = "https://fcc-weather-api.glitch.me/api/current?";
+
+  navigator.geolocation.getCurrentPosition(success, error);
+
+  function success(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+
+    location.innerHTML =
+      "Latitude is " + latitude + "° Longitude is " + longitude + "°";
+
+    fetch(url + "lat=" + latitude + "&lon=" + longitude)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        console.log(JSON.stringify(data));
+        document.getElementById("root").innerHTML = data.coord.lon;
+        return data;
+      })
+      .catch(function(error) {
+        console.error("error");
+      });
+  }
+
+  function error() {
+    location.innerHTML = "Unable to retrieve your location";
+  }
+
+  location.innerHTML = "Locating...";
+}
+
+userLocation();
