fae441246ae1236f86c2c80b563e27b5c02d90bb
RebeccaSeraphine <rebecca.s.mueller@web.de>
2019-09-22 13:18:02 +0200

    comments & README Questions

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 function userLocation() {
   var location = document.getElementById("location");
 
+  // Get location
   navigator.geolocation.getCurrentPosition(success, error);
 
   function success(position) {
     lat = position.coords.latitude;
     lon = position.coords.longitude;
-
+    // Insert Coordinates
     location.innerHTML = "Latitude: " + lat + "°   Longitude: " + lon + "°";
 
+    // Fetch weather data from API for the coordinates
     fetch(
       "https://fcc-weather-api.glitch.me/api/current?lon=" +
         lon +
@@ -31,26 +33,11 @@ function userLocation() {
       })
       .catch(function(error) {
         console.log("Fetch Error", error);
+        // Frequent error
         if (response.status === 429) {
           alert("Error 429: Too many requests");
         }
       });
-    /*
-      .then(function(response) {
-
-        return response.json();
-      })
-      .then(function(data) {
-        data = JSON.stringify(data);
-        console.log(data);
-        dataObject = JSON.parse(data); //this.data?
-        console.log(dataObject);
-        WeatherInfo(dataObject);
-        return dataObject;
-      })
-      .catch(function(error) {
-        console.error(error);
-      });*/
   }
 
   function error() {
@@ -58,11 +45,12 @@ function userLocation() {
       "Unable to retrieve your location, <br/> please ensure location access is allowed.";
   }
 
-  location.innerHTML = "Loading your location...";
+  location.innerHTML = "Loading your location..."; // Placeholder while loding
 }
 
 userLocation();
 
+// Weather information visualization
 function WeatherInfo(data) {
   var weatherIcon = document.getElementById("weatherIcon");
   var city = document.getElementById("city");
@@ -70,10 +58,12 @@ function WeatherInfo(data) {
   var wind = document.getElementById("wind");
 
   var weather = data.weather;
-  console.log(weather);
 
+  /*
+  console.log(weather);
   console.log(weather[0].icon);
   console.log(data.name);
+  */
 
   weatherIcon.src = weather[0].icon;
   city.innerHTML = data.name;
